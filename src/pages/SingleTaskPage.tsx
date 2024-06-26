85a984a34cd48f92fdc0a580ab986fadeb7a5220
serhiisezonchyk <sergey040901@gmail.com>
2024-06-26 14:01:16 +0300

    feat: adding page of single task/prepare

--- src/pages/SingleTaskPage.tsx
+++ src/pages/SingleTaskPage.tsx
@@ -4,9 +4,16 @@ import { useParams } from 'react-router-dom';
 const SingleTaskPage = () => {
   const { taskId } = useParams();
   if (!taskId) return;
-  const { data: task, isFetching:isTaskLoading, isError } = useTask(taskId);
-  
-  return <div>{task?.title}</div>;
+  const { data: task, isFetching: isTaskLoading, isError } = useTask(taskId);
+
+  return (
+    <div className="py-4">
+      <div className="container">
+        <h1>Title: {task?.title}</h1>
+        <p>{task?.content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SingleTaskPage;
